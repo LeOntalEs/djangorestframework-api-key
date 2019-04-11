5d5cfc5614a69fbca3ca8e0a12b534baded7ab18
taratep <taratep.sira@gmail.com>
2019-04-11 15:40:20 +0700

    example scope API ViewSet

--- example_project/animals/views.py
+++ example_project/animals/views.py
@@ -1,11 +1,11 @@
 """Example views."""
 
-from rest_framework import viewsets
-
+from rest_framework import viewsets, views, response, mixins, exceptions
 
 from .models import Animal
 from .serializers import AnimalSerializer
-from rest_framework_api_key.permissions import HasAPIKey
+from rest_framework_api_key.utilities import get_token_from_request, get_api_key
+from rest_framework_api_key.permissions import HasAPIKey, HasSomeScopeInAPIKey
 
 
 class AnimalViewSet(viewsets.ReadOnlyModelViewSet):
@@ -14,3 +14,16 @@ class AnimalViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
     permission_classes = (HasAPIKey,)
+
+
+class AnimalCreateViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    """ Endpoints for create animals with verify api key scope."""
+
+    queryset = Animal.objects.all()
+    serializer_class = AnimalSerializer
+    permission_classes = (HasSomeScopeInAPIKey, )
+    scopes = ('add-animal',)  # needed scope for access this view
+
+    def permission_denied(self, request, message="Permission denied"):
+        """ override for PermissionDenied message """
+        raise exceptions.PermissionDenied(detail=message)
